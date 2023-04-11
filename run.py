d1becc211122b828deb2e07203e262f39b84c6aa
Adam Toman <adamtoman1@aol.com>
2023-04-11 10:33:34 +0000

    Add list of words to be selected at random

--- run.py
+++ run.py
@@ -51,4 +51,16 @@ HANGMAN_PICS = ['''
        |
 =========''']
 
-
+words = "basketball football baseball soccer tennis golf hockey \
+        volleyball rugby badminton tabletennis swimming athletics \
+        gymnastics wrestling boxing cycling skiing snowboarding \
+        archery shooting fencing rowing kayaking canoeing diving \
+        weightlifting powerlifting bodybuilding triathlon marathon \
+        pentathlon decathlon surfing skateboarding rockclimbing \
+        mountaineering fishing hunting horsebackriding \
+        polo squash racquetball lacrosse ultimatefrisbee \
+        kickboxing karate taekwondo judo aikido capoeira \
+        jiu-jitsu muaythai crossfit parkour calisthenics bocce croquet \
+        curling darts billiards bowling skating sledding \
+        tobogganing bobsleigh luge snowmobiling \
+        kiteboarding windsurfing parasailing".split()
